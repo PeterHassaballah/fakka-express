4ede7499420f39c2e41229a663a2e8bf2bd777ba
PeterHassaballah <peter.hassaballah@gmail.com>
2019-12-09 18:48:41 +0200

    disable default head

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+  // res.writeHead(200, { 'Content-Type': 'text/html' });
   res.render('index', { title: 'Express' });
 });
 
