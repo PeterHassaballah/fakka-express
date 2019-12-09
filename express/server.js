7ae920d0b6581072223db8be2b0547df8341749d
PeterHassaballah <peter.hassaballah@gmail.com>
2019-12-09 18:54:53 +0200

    disable redirect

--- express/server.js
+++ express/server.js
@@ -50,7 +50,8 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 
 
